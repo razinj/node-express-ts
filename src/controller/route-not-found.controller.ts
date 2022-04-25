121cc5936055a2c890267960dc6e03a2a020f311
razinj <nizarjailane@gmail.com>
2022-04-25 22:31:12 +0000

    Fixed 404 controller

--- src/controller/route-not-found.controller.ts
+++ src/controller/route-not-found.controller.ts
@@ -2,7 +2,7 @@ import { Request, Response } from 'express'
 import { CustomResponse } from '../model/custom-response'
 
 const routeNotFoundHandler = (req: Request, res: Response): void => {
-  res.status(200).json(new CustomResponse(null, null, 'Route requested not found!'))
+  res.status(404).json(new CustomResponse(null, null, 'Route requested not found!'))
 }
 
 export default { routeNotFoundHandler }
